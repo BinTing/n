4a597a82bcd5c99c0900f4d832b647a4dd407908
ccsfedu <60402146+ccsfedu@users.noreply.github.com>
2020-03-02 01:12:54 +0800

    1
    1

---
+++ .img/lucifiles/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/tony.lua
@@ -0,0 +1,21 @@
+module("luci.controller.admin.tony",package.seeall)
+function index()
+entry({"admin","tony"},alias("admin","tony","tony"),_("✪Tony"),80).index=true
+entry({"admin","tony","Control"},cbi("admin_tony/cbi_tony_gfw"),_("① OpenSesame"),1)
+entry({"admin","tony","GCP"},template("admin_tony_view/gcp"),_("② GoogleCloud"),2)
+entry({"admin","system","reboot","call"},post("action_reboot"))
+entry({"admin","tony","VPNVPS"},template("admin_tony_view/vpnvps"),_("③ VPNVPS"),3)
+entry({"admin","tony","FreeSSR"},template("admin_tony_view/freessr"),_("④ Ladder"),4)
+entry({"admin","tony","Decode"},template("admin_tony_view/linkdecode"),_("⑤ Decode"),5)
+entry({"admin","tony","Reboot"},template("admin_tony_view/reboot"),_("⑥ Reboot"),6)
+entry({"admin","system","reboot","call"},post("action_reboot"))
+entry({"admin","tony","Telegram"},template("admin_tony_view/telegram"),_("➆ Telegram"),7)
+entry({"admin","tony","Poweroff"},template("admin_tony_view/poweroff"),_("➇ Poweroff"),8)
+entry({"admin","system","poweroff","call"},post("action_poweroff"))
+entry({"admin","tony","Upgrade"},cbi("admin_tony/cbi_tony_ssd"),_("★ 一键刷机ToSSD"),11)
+entry({"admin","tony","StickyNotes"},template("admin_tony_view/StickyNotes"),_("➈ StickyNotes"),12)
+entry({"admin","tony","docker"},cbi("docker"),_("★ Docker"),13)
+entry({"admin","services","docker","status"},call("act_status")).leaf=true
+entry({"admin","tony","theme"},cbi("admin_tony/cbi_tony_theme"),_("★ Theme"),14)
+entry({"admin","tony","logo"},cbi("admin_tony/cbi_tony_logo"),_("★ Logo show"),15)
+end
